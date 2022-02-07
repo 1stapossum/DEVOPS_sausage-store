8d0780b7922cff680abe13bf9fe31abbf748c3a3
Alexandr V. Volokhov <antinitrino@yandex.ru>
2022-02-07 22:23:08 +0300

    6

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -6,6 +6,8 @@ sudo cp -rf sausage-store-front.service /etc/systemd/system/sausage-store-front.
 #sudo rm -f /home/jarservice/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /home/student/sausage-store.tar.gz ${NEXUS_FRONTEND_REPO_URL}/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
+rm -rf sausage-store-front
+mkdir sausage-store-front
 tar xzf sausage-store.tar.gz
 cp sausage-store-${VERSION}/public_html/* sausage-store-front
 rm -rf sausage-store-${VERSION}
