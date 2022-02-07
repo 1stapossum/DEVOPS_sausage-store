a05f56ebd5d816902b92681e3560962120b7dd95
Alexandr V. Volokhov <antinitrino@yandex.ru>
2022-02-07 22:35:16 +0300

    7

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -14,5 +14,5 @@ rm -rf sausage-store-${VERSION}
 #sudo cp ./sausage-store.jar /home/student/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
 #Обновляем конфиг systemd с помощью рестарта!!!
 sudo systemctl daemon-reload
-#Перезапускаем сервис сосисочной
+#Перезапускаем сервис сосисочной!
 sudo systemctl restart sausage-store-front
