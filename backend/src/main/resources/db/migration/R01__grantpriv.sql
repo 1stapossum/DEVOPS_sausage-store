9f1d7eb360ccfc915783932d05c98ac0b3dd9749
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-03-07 15:38:10 +0300

    2

---
+++ backend/src/main/resources/db/migration/R01__grantpriv.sql
@@ -0,0 +1,3 @@
+GRANT select ON * to read_user;
+GRANT select,insert ON * to write_user;
+GRANT ALL ON * to admin_user;
---
+++ backend/src/main/resources/db/migration/V02__alter.sql
@@ -0,0 +1,2 @@
+ALTER COLUMN 'name' TYPE character varying(20);
+ALTER COLUMN 'picture_url' TYPE character varying(100);
