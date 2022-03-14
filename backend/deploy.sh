d75573da23aeaf7f870ae6257d0f0b6594e0498b
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-03-14 16:26:05 +0300

    94

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -9,5 +9,5 @@ curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /home/student/sausage-store.jar
 #sudo cp ./sausage-store.jar /home/student/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
-#Перезапускаем сервис сосисочной!!!!!
+#Перезапускаем сервис сосисочной!!!!!1
 sudo systemctl restart sausage-store
