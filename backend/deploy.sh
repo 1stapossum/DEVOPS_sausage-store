855dda67e11d528f71586b19332ade1857d67bc8
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-03-19 15:16:29 +0300

    HIDE Sensedata#4

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,10 +4,10 @@ set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store.service /etc/systemd/system/sausage-store.service
 
-sudo touch /home/student/sausage-store-env
-sudo env | grep MONGO_PASSWORD > /home/student/sausage-store-env
-sudo env | grep PSQL_DB_PASSWORD >> /home/student/sausage-store-env
-sudo cp -rf sausage-store-env /etc/systemd/system
+#sudo touch /home/student/sausage-store-env
+#sudo env | grep MONGO_PASSWORD > /home/student/sausage-store-env
+#sudo env | grep PSQL_DB_PASSWORD >> /home/student/sausage-store-env
+sudo cp -rf sausage-store-env /etc/systemd/system/sausage-store-env
 
 #sudo rm -f /home/jarservice/sausage-store.jar||true
 #Переносим артефакт в нужную папку
