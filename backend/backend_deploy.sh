bd7a664a7594b534ce3f7ec963e4c5710989a5f1
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-03-21 19:06:49 +0300

    13

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -4,7 +4,7 @@ cat > .env <<EOF
 SPRING_DATASOURCE_URL=${PSQL_HOST}
 SPRING_DATASOURCE_USERNAME=${PSQL_USER}
 SPRING_DATASOURCE_PASSWORD=${PSQL_DB_PASSWORD}
-SPRING_DATA_MONGODB_URI=mongodb://antinitrino:${MONGO_PASSWORD}@rc1b-nczpq63snuc5a3z2.mdb.yandexcloud.net:27018/antinitrino?tls=true
+SPRING_DATA_MONGODB_URI=${MONGO_URI_WHOLE}
 EOF
 #docker network create -d bridge sausage_network || true
 docker pull ${$CI_REGISTRY}/sausage-store/sausage-backend:latest
