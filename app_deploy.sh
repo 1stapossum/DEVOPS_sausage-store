bf1811fb86a37f553713d54b121c170003e7e36f
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-04-06 18:11:39 +0300

    app_deploy_13

--- app_deploy.sh
+++ app_deploy.sh
@@ -17,22 +17,35 @@ docker rmi sausage-backend || true
 
 docker stop backend-report || true
 docker rm backend-report || true
-docker rmi  || backend-report true
+docker rmi  backend-report true
 
-
-docker rm vault -f || true
-
-
-#docker create --cap-add=IPC_LOCK --name vault -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -e 'VAULT_SERVER=http://127.0.0.1:8200' -e 'VAULT_ADDR=http://127.0.0.1:8200' vault
-##
-docker run -d --cap-add=IPC_LOCK --name vault -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -e 'VAULT_SERVER=http://127.0.0.1:8200' -e 'VAULT_ADDR=http://127.0.0.1:8200' vault
-
-#cat >  vault_params <<\eof
-cat <<EOF | docker exec -i vault ash
-  sleep 10;
-  vault login myroot
-  vault kv put secret/sausage-store spring.datasource.password=${PSQL_DB_PASSWORD} spring.datasource.username=${PSQL_USER} spring.data.mongodb.uri=${MONGO_URI_WHOLE}
-EOF
-
+docker stop vault || true
+docker rm vault || true
+docker rmi vault ||true
 
 docker-compose -d up
+
+
+#cat <<EOF | docker exec -i vault ash
+#  sleep 5;
+#  vault login myroot
+#  vault kv put secret/sausage-store spring.datasource.password="PSQL_DB_PASSWORD" spring.datasource.username="PSQL_USER" spring.data.mongodb.uri="MONGO_URI_WHOLE"
+#EOF
+
+cat > ihatevault.sh << EOFF
+#!/usr/bin/bash
+cat << EOF | docker exec -i vault ash
+  sleep 5;
+  vault login ${VAULT_TOKEN}
+  vault kv put secret/sausage-store spring.datasource.username=${PSQL_USER} \
+  spring.datasource.password=${PSQL_DB_PASSWORD} \
+  spring.data.mongodb.uri=${MONGO_URI_WHOLE}
+EOF
+EOFF
+
+chmod +x ihatevault.sh
+sleep 15
+bash ihatevault.sh
+sleep 5
+rm -f ihatevault.sh
+
