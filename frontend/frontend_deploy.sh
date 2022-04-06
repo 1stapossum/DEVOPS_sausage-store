062d2966dd2a09813ba2d4b30bafeea2901507b4
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-04-06 15:12:27 +0300

    app_deploy_7

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -17,4 +17,4 @@ set -e
 #docker run -d --name sausage-bacfrontend --network=sausage_network --pull always --restart always --env-file .env ${CI_REGISTRY_IMAGE}/sausage-frontend:latest
 docker run -d --name sausage-frontend --network=sausage_network --pull always --restart always  -p 80:80 ${CI_REGISTRY_IMAGE}/sausage-frontend:latest
 
-    #--pull always!!
+    #--pull always!
