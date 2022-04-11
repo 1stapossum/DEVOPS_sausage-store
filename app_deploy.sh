d32f3f7d6aa1917c1c6094b9e6e060c664dbe014
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-04-11 14:41:57 +0300

    28

--- app_deploy.sh
+++ app_deploy.sh
@@ -7,6 +7,7 @@ sleep 5
 docker-compose pull
 docker-compose up -d
 docker-compose up -d --scale backend=2
+docker exec -d  sausage-frontend docker-gen -only-exposed -watch -notify "/etc/init.d/nginx reload" /app/proxytemplate /etc/nginx/nginx.conf
 
 cat > ihatevault.sh << EOFF
 #!/usr/bin/bash
