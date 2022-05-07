902b2f7870644465ed0e1149a28ce59a4a1049f1
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-05-07 11:58:57 +0300

    b/g 2

--- app_deploy.sh
+++ app_deploy.sh
@@ -4,8 +4,8 @@ docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 
 #sleep 5
-#docker-compose pull
-#docker-compose up -d --scale backend=2
+docker-compose pull
+docker-compose up -d
 ### BLUE/GREEN
 #source var
 #CONTAINER_RUN_CHECK=$(docker container  ls -q )
@@ -15,34 +15,28 @@ CONTAINER_RUN_CHECK_GREEN=$(docker ps  -q --filter="name=green")	#GREEN
 
 if [[ $CONTAINER_RUN_CHECK_ALL == *"blue"* ]]; then
   echo "Blue backend runing"
-#\  docker-compose stop $CONTAINER_RUN_CHECK_GREEN
-echo "Stoping green"
-#  docker stop $(docker ps  -q --filter="name=green")
+  echo "Stoping green"
 docker-compose stop backend-green
 docker-compose rm -f backend-green 
 docker-compose pull backend-green #budem schitat chto pull
-echo "Starting Green"
-  docker-compose up --scale backend-green=1 -d
+  echo "Starting Green"
+docker-compose up --scale backend-green=1 -d
 
-connand=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=green"))
-until [ "$connand" == "healthy" ] 
+command=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=green"))
+until [ "$command" == "healthy" ] 
 do
-
     sleep 0.1;
-connand=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=green"))
-	echo $connand;
+command=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=green"))
+	echo $command;
 done
- docker-compose up --scale backend-green=2 -d
-echo "Stoping blue!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+docker-compose up --scale backend-green=2 -d
+  echo "Stoping blue!"
 #  docker stop $(docker ps  -q --filter="name=blue")a
 docker-compose stop backend-blue
 docker-compose rm -f backend-blue
 
-
-
-
 elif [[ $CONTAINER_RUN_CHECK_ALL == *"green"* ]]; then
-  echo "Green back run"
+  echo "Green backend runing"
 
 echo "Stoping blue"
 #  docker stop $(docker ps  -q --filter="name=blue")
@@ -52,13 +46,13 @@ docker-compose pull backend-blue #budem schitat chto pull
 echo "Starting blue"
   docker-compose up --scale backend-blue=1 -d
 
-connand=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=blue"))
-until [ "$connand" == "healthy" ]
+command=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=blue"))
+until [ "$command" == "healthy" ]
 do
 
     sleep 0.1;
-connand=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=blue"))
-        echo $connand;
+command=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=blue"))
+        echo $command;
 done
  docker-compose up --scale backend-blue=2 -d
 
