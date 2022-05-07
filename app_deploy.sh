11270322950231f2593e1af4448f496f83bd29bd
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-05-07 12:17:40 +0300

    b/g 3

--- app_deploy.sh
+++ app_deploy.sh
@@ -6,6 +6,25 @@ docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 #sleep 5
 docker-compose pull
 docker-compose up -d
+cat > ihatevault.sh << EOFF
+#!/usr/bin/bash
+cat << EOF | docker exec -i vault ash
+  sleep 10
+  vault login ${VAULT_DEV_ROOT_TOKEN_ID}
+  vault kv put secret/sausage-store spring.datasource.username=${SPRING_DATASOURCE_USERNAME} \
+  spring.datasource.password=${SPRING_DATASOURCE_PASSWORD} \
+  spring.data.mongodb.uri=${SPRING_DATA_MONGODB_URI}
+EOF
+EOFF
+
+chmod +x ihatevault.sh
+sleep 15
+bash ihatevault.sh
+
+docker exec -d  sausage-frontend docker-gen -only-exposed -watch -notify "/etc/init.d/nginx reload" /app/proxytemplate /etc/nginx/nginx.conf
+
+
+
 ### BLUE/GREEN
 #source var
 #CONTAINER_RUN_CHECK=$(docker container  ls -q )
@@ -44,42 +63,21 @@ docker-compose stop backend-blue
 docker-compose rm -f backend-blue
 docker-compose pull backend-blue #budem schitat chto pull
 echo "Starting blue"
-  docker-compose up --scale backend-blue=1 -d
-
+docker-compose up --scale backend-blue=1 -d
 command=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=blue"))
 until [ "$command" == "healthy" ]
 do
-
     sleep 0.1;
 command=$(docker inspect -f {{.State.Health.Status}} $(docker ps  -q --filter="name=blue"))
         echo $command;
 done
  docker-compose up --scale backend-blue=2 -d
-
-
 echo "Stoping green"
-#  docker stop $(docker ps  -q --filter="name=green")
 docker-compose stop backend-green
 docker-compose rm -f backend-green
-
-
-
 fi
-#echo $CONTAINER_RUN_CHECK
-docker exec -d  sausage-frontend docker-gen -only-exposed -watch -notify "/etc/init.d/nginx reload" /app/proxytemplate /etc/nginx/nginx.conf
 
-cat > ihatevault.sh << EOFF
-#!/usr/bin/bash
-cat << EOF | docker exec -i vault ash
-  sleep 10
-  vault login ${VAULT_DEV_ROOT_TOKEN_ID}
-  vault kv put secret/sausage-store spring.datasource.username=${SPRING_DATASOURCE_USERNAME} \
-  spring.datasource.password=${SPRING_DATASOURCE_PASSWORD} \
-  spring.data.mongodb.uri=${SPRING_DATA_MONGODB_URI}
-EOF
-EOFF
 
-chmod +x ihatevault.sh
-sleep 15
-bash ihatevault.sh
+
+
 
